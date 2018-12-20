95741916f9a11c9e99bbb5f0a5b05dfe7d840de5
cristina barriga <c.barrigaradiologia@gmail.com>
2018-12-20 15:02:50 +0100

    añadido get id usuario

--- src/main/java/com/squirrelTail/services/UsuariosService.java
+++ src/main/java/com/squirrelTail/services/UsuariosService.java
@@ -15,7 +15,7 @@ import com.squirrelTail.persistencia.UsuarioManager;
 
 @Path("/usuarios")
 public class UsuariosService {
-	
+
 	@GET
 	@Produces("application/json")
 	public Response getUsuarios() {
@@ -23,15 +23,15 @@ public class UsuariosService {
 		UsuarioManager um;
 		try {
 			um = UsuarioManager.getInstance();
-			
+
 			return Response.status(200).entity(um.getUsuarios()).build();
 		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 			return Response.status(500).entity(new StatusMessage(500, "error")).build();
 		}
 	}
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -42,11 +42,11 @@ public class UsuariosService {
 			UsuarioManager um;
 			try {
 				um = UsuarioManager.getInstance();
-				
+
 				resp = Response.status(200).entity(um.createUsuario(usuarioNuevo)).build();
 			} catch (Exception e) {
 				e.printStackTrace();
-				
+
 				resp = Response.status(500).entity(new StatusMessage(500, "error")).build();
 			}
 		} else {
@@ -55,29 +55,22 @@ public class UsuariosService {
 
 		return resp;
 	}
-	
-//	@Path("/{nid}")
-//	@GET
-//	@Produces("application/json")
-//	public Response getPedido(@PathParam("nid") int nid) {
-//		Response resp = null;
-//		Pedido pedidoRet = null;
-//
-//		for (Pedido pedido : pedidos) {
-//			if (pedido.getPid() == pid) {
-//				pedidoRet = pedido;
-//				break;
-//			}
-//		}
-//
-//		if (pedidoRet == null) {
-//			resp = Response.status(404).entity(new StatusMessage(404, "El pedido no existe")).build();
-//		} else {
-//			resp = Response.status(200).entity(pedidoRet).build();
-//		}
-//
-//		return resp;
-//	}
-//
+
+	@Path("/{nid}")
+	@GET
+	@Produces("application/json")
+	public Response getUsuarios(@PathParam("nid") int nid) {
+
+		UsuarioManager um;
+		try {
+			um = UsuarioManager.getInstance();
+
+			return Response.status(200).entity(um.getUsuario(nid)).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return Response.status(500).entity(new StatusMessage(500, "error")).build();
+		}
+	}
 
 }
