5e6139c59ac84a3df18064cb3a35f259366bc884
cristina barriga <c.barrigaradiologia@gmail.com>
2018-12-21 12:40:02 +0100

    PUT añadido

--- src/main/java/com/squirrelTail/persistencia/UsuarioManager.java
+++ src/main/java/com/squirrelTail/persistencia/UsuarioManager.java
@@ -35,26 +35,38 @@ public class UsuarioManager {
 		return listaUsuarios;
 	}
 	
-	public Usuario createUsuario(Usuario newE) throws Exception {
+	public Usuario createUsuario(Usuario newU) throws Exception {
 		Session session = sf.openSession();
 		Transaction t = session.beginTransaction();
 
-		int nid = ((Integer) session.save(newE)).intValue();
-		newE.setNid(nid);
+		int nid = ((Integer) session.save(newU)).intValue();
+		newU.setNid(nid);
 		
 		t.commit();
 		session.close();
 
-		return newE;
+		return newU;
 	}
 
 	public Usuario getUsuario(int nid) throws Exception {
 		Session session = sf.openSession();
 
-		Usuario recE = session.get(Usuario.class, nid);
+		Usuario recU = session.get(Usuario.class, nid);
 
 		session.close();
 
-		return recE;
+		return recU;
+	}
+
+	public Object updateUsuario(Usuario actU) throws Exception {
+		Session session = sf.openSession();
+		Transaction t = session.beginTransaction();
+
+		session.update(actU);
+		
+		t.commit();
+		session.close();
+
+		return actU;
 	}
 }
--- src/main/java/com/squirrelTail/services/UsuariosService.java
+++ src/main/java/com/squirrelTail/services/UsuariosService.java
@@ -3,6 +3,7 @@ package com.squirrelTail.services;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -72,5 +73,32 @@ public class UsuariosService {
 			return Response.status(500).entity(new StatusMessage(500, "error")).build();
 		}
 	}
+	
+	@Path("/{nid}")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response actualizarUsuario(@PathParam("nid") int nid, Usuario unUsuario) {
+		Response resp = null;
+
+		unUsuario.setNid(nid);
+		if (unUsuario.validate()) {
+			UsuarioManager um;
+			try {
+				um = UsuarioManager.getInstance();
+
+				resp = Response.status(200).entity(um.updateUsuario(unUsuario)).build();
+			} catch (Exception e) {
+				e.printStackTrace();
+
+				resp = Response.status(500).entity(new StatusMessage(500, "error")).build();
+			}
+		} else {
+			resp = Response.status(400).entity(new StatusMessage(400, "Usuario incompleto")).build();
+		}
+
+		return resp;
+	}
+
 
 }
